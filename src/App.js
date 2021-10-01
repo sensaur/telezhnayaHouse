7b83a6d2463d8c30a1fd7add4a8ee5ba5211ec6f
Sensaur <zhigarev@bk.ru>
2021-10-01 08:45:51 +0300

    Services component added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./pages/Home"
+import SingleRoom from "./pages/SingleRoom"
+import Rooms from "./pages/Rooms"
+import Error from "./pages/Error"
+import NavBar from "./components/NavBar";
+import {Route, Switch} from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <NavBar/>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/rooms' component={Rooms}/>
+                <Route exact path='/rooms/:slug' component={SingleRoom}/>
+                <Route component={Error}/>
+            </Switch>
+        </>
+    );
+}
+
+export default App;
