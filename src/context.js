6a53fda7004abcaa3ecf6e94b7e7eca956465c23
Sensaur <zhigarev@bk.ru>
2021-10-10 21:00:28 +0300

    Services component added

--- src/context.js
+++ src/context.js
@@ -26,7 +26,7 @@ class RoomProvider extends Component {
 
     formatData(items) {
         let tempItems = items.map(item => {
-            let id  = item.sys.id;
+            let id = item.sys.id;
             let images = item.fields.images.map(image => image.fields.file.url);
             let room = {...item.fields, images, id}
             return room
@@ -34,9 +34,15 @@ class RoomProvider extends Component {
         return tempItems
     }
 
+    getRoom = (slug) => {
+        let tempRooms = [...this.state.rooms]
+        const room = tempRooms.find((room) => room.slug === slug)
+        return room
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{...this.state}}>
+            <RoomContext.Provider value={{...this.state, getRoom: this.getRoom()}}>
                 {this.props.children}
             </RoomContext.Provider>
         );
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import defaultBCG from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from "../context";
+
 
 class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props)
+    }
+
+    // componentDidMount() {}
+
     render() {
         return (
             <div>
