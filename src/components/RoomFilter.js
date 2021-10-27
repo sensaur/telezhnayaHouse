6e193358c7441e44a49a587069e64c000f4ee6f8
Sensaur <zhigarev@bk.ru>
2021-10-27 23:22:01 +0300

    functioning

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -41,6 +41,40 @@ function RoomFilter({rooms}) {
                     </select>
                 </div>
                 {/*end select guest*/}
+                {/*room price*/}
+                <div className='form-group'>
+                    <label htmlFor='price'>
+                        room price ${price}
+                    </label>
+                    <input type="range" name='price' min={minPrice} max={maxPrice} id='price' value={price}
+                           onChange={handleChange} className='form-control'/>
+                </div>
+                {/*end room price*/}
+                {/*size*/}
+                <div className='form-group'>
+                    <label htmlFor='size'>room size
+                    </label>
+                    <input type='number' name='minSize' id='size' value={minSize} onChange={handleChange}
+                           className='size-input'/>
+                    <input type='number' name='maxSize' id='size' value={maxSize} onChange={handleChange}
+                           className='size-input'/>
+                </div>
+                {/*end of size*/}
+                {/*extras*/}
+                <div className='form-group'>
+                    <div className='single-extra'>
+                        <input type="checkbox" name='breakfast' id='breakfast' checked={breakfast}
+                               onChange={handleChange}/>
+                        <label htmlFor='breakfast'>breakfast</label>
+                    </div>
+                    <div className='single-extra'>
+                        <input type="checkbox" name='pets' id='pets' checked={pets}
+                               onChange={handleChange}/>
+                        <label htmlFor='pets'>pets</label>
+                    </div>
+                </div>
+                {/*end of extras*/}
+
             </form>
         </section>
     );
--- src/context.js
+++ src/context.js
@@ -25,7 +25,7 @@ class RoomProvider extends Component {
         let rooms = this.formatData(items)
         let featuredRooms = rooms.filter(room => room.featured === true)
         let maxPrice = Math.max(...rooms.map(item => item.price))
-        let maxSize = Math.max(...rooms.map(item => item.price))
+        let maxSize = Math.max(...rooms.map(item => item.size))
         this.setState({
             rooms,
             featuredRooms,
@@ -56,7 +56,7 @@ class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target
-        const value = event.type === 'checkbox' ? target.checked : target.value
+        const value = target.type === 'checkbox' ? target.checked : target.value
         const name = event.target.name
         this.setState({
                 [name]: value,
@@ -70,11 +70,31 @@ class RoomProvider extends Component {
 
     filterRooms = () => {
         let {rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state
+        // all the rooms
         let tempRooms = [...rooms]
-
+        // transform values
+        capacity = parseInt(capacity)
+        //filter by type
         if (type !== "all") {
             tempRooms = tempRooms.filter(room => room.type === type)
         }
+        //filter by capacity
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+        //filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price)
+        //filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize & room.size <= maxSize)
+        //filter by breakfast
+        if(breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+        //filter by pets
+        if(pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
+        //change state
         this.setState({
             sortedRooms: tempRooms
         })
