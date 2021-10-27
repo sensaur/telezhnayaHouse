b877996223585bdf65862d2a14eb13a7044ca56e
Sensaur <zhigarev@bk.ru>
2021-10-27 22:11:05 +0300

    guests

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -16,18 +16,31 @@ function RoomFilter({rooms}) {
     types = types.map((item, index) => {
         return <option value={item} key={index}>{item}</option>
     })
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+        return <option key={index} value={item}>{item}</option>
+    })
     return (
         <section className='filter-container'>
             <Title title="search rooms"/>
             <form className="filter-form">
                 {/*select type*/}
                 <div className="form-group">
-                    <label htmlFor="type"> room type</label>
+                    <label htmlFor="type">room type</label>
                     <select name='type' id='type' value={type} className="form-control" onChange={handleChange}>
                         {types}
                     </select>
                 </div>
                 {/*end select type*/}
+                {/*select guest*/}
+                <div className="form-group">
+                    <label htmlFor="capacity">Guests</label>
+                    <select name='capacity' id='capacity' value={capacity} className="form-control"
+                            onChange={handleChange}>
+                        {people}
+                    </select>
+                </div>
+                {/*end select guest*/}
             </form>
         </section>
     );
