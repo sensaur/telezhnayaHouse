963710ce9b981d32d97599a5190df0f0887e7ed5
Sensaur <zhigarev@bk.ru>
2021-10-28 00:10:22 +0300

    functioning

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ function Banner({children, title, subtitle}) {
     return (
         <div className='banner'>
             <h1>{title}</h1>
+            <div></div>
             <p>{subtitle}</p>
             {children}
         </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,6 +3,7 @@ function Title({title}) {
     return (
         <div className="section-title">
             <h4>{title}</h4>
+            <div />
         </div>
     )
 }
